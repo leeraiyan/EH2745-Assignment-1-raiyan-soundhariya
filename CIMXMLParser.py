af7227cea5025404c644032d2f3840b88c631c91
leeraiyan <woblek@gmail.com>
2023-05-12 10:22:49 +0300

    initial commit

---
+++ CIMXMLParser.py
@@ -0,0 +1,195 @@
+import re
+import xml.etree.ElementTree as ET
+
+ns = {'cim':'http://iec.ch/TC57/2013/CIM-schema-cim16#',
+      'entsoe':'http://entsoe.eu/CIM/SchemaExtension/3/1#',
+      'rdf':'{http://www.w3.org/1999/02/22-rdf-syntax-ns#}'}
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex):
+        self.vertices[vertex] = []
+
+    def add_edge(self, source, destination):
+        self.vertices[source].append(destination)
+        self.vertices[destination].append(source)
+
+    def get_adjacent_vertices(self, vertex):
+        return self.vertices[vertex]
+
+
+class CIM_XML_parser:
+    def __init__(self, eqPath, sshPath = None) -> None:
+
+        
+        
+        #the two files of the network
+        eqTree = ET.parse(eqPath)
+        self.eqRoot = eqTree.getroot()
+        sshTree = ET.parse(sshPath)
+        self.sshRoot = sshTree.getroot()
+
+        #graph of the network
+        self.networkGraph = Graph()
+
+        self.equipmentIDtoType = {}
+
+        self.equipment = {
+            "Substation": {},
+            "BaseVoltage":{},
+            "VoltageLevel":{},
+            "GeneratingUnit":{},
+            "SynchronousMachine":{},
+            "RegulatingControl":{},
+            "PowerTransformer":{},
+            "EnergyConsumer":{},
+            "PowerTransformerEnd":{},
+            "Breaker":{},
+            "RatioTapChanger":{},
+            "Region":{},
+            "EquipmentContainer":{},
+            "ACLineSegment":{},
+            "BusbarSection":{},
+            "ConnectivityNode":{},
+            "Terminal":{},
+            "Line":{},
+            "ConnectivityNodeContainer":{},
+            "LinearShuntCompensator":{},
+            "RatioTapChanger":{},
+            "PhaseTapChangerAsymetrical":{},
+            "EnergySource":{},
+            "EquivalentInjection":{},
+            "PetersenCoil":{},
+            "CurrentLimit": {},
+            "OperationalLimitSet": {}
+        }
+
+    def search_key(self, key):
+        if key in self.equipment:
+            return self.equipment[key]
+        else:
+            for value in self.equipment.values():
+                if isinstance(value, dict):
+                    result = self.search_key(value, key)
+                    if result is not None:
+                        return result
+        print("Nothing Related")
+
+    def getEquipmentDict(self, equipmentID):
+        key = self.getEquipmentType(equipmentID)
+        return self.equipment[key][equipmentID]
+    
+    def getEquipmentType(self, equipmentID):
+        try:
+            return self.equipmentIDtoType[equipmentID]
+        except:
+            return None
+        
+    def associateTerminaltoEquipment(self):
+        counter = 0
+        for terminal, terminalInfo in self.equipment["Terminal"].items():
+            counter += 1
+            equipmentID = terminalInfo["Terminal.ConductingEquipment"].replace("#", "")
+            equipmentType = self.getEquipmentType(equipmentID)
+            self.equipment[equipmentType][equipmentID]["associatedTerminals"].append(terminal)
+            # print(counter, "/", len(self.equipment["Terminal"]), "Associated Terminal", terminal, "with Equipment", equipmentID, self.getEquipmentType(equipmentID))
+        return None
+
+    def investigate(self, equipmentID):
+        dictionary = self.getEquipmentDict(equipmentID)
+
+        print(self.getEquipmentType(equipmentID))
+        for key, value in dictionary.items():
+            if len(value) > 0:
+                if value[0] == '_':
+                    print(re.sub(r'^.*?\.', '', key), value , "<-" , self.getEquipmentType(value.replace("#", "")))
+
+                else:
+                    print(re.sub(r'^.*?\.', '', key), value)
+        return None
+
+    def runEQ(self):
+        for node in self.eqRoot:
+            #check equipment type
+            # logging.info(node.tag.replace("{"+ns['cim']+"}",""))
+            equipmentType = node.tag.replace("{"+ns['cim']+"}","")
+
+            #obtain ID of equipment first
+            newKey = node.attrib.get(ns['rdf']+'ID')
+
+            
+            # logging.info("ID:", node.attrib.get(ns['rdf']+'ID'))
+
+            #empty dictionary for information of the equipment above
+            newValue = {}
+            #populate empty dictionary with information if it is an important equipment
+            if equipmentType in self.equipment.keys():
+                for leaf in node:
+
+                    #this is an rdf resource
+                    if leaf.text == None:
+                        newValue[leaf.tag.replace("{"+ns['cim']+"}","")] = leaf.attrib.get(ns['rdf']+'resource').replace("#", "")
+                        # logging.info(leaf.tag.replace("{"+ns['cim']+"}",""), leaf.attrib.get(ns['rdf']+'resource'))
+                    
+                    #this is just normal text information
+                    else:
+                        # print(re.sub(r"\b{http\S*", "", leaf.tag), leaf.text
+                        newValue[leaf.tag.replace("{"+ns['cim']+"}","").replace("{"+ns['entsoe']+"}","")] = leaf.text
+                        # logging.info(leaf.tag.replace("{"+ns['cim']+"}","").replace("{"+ns['entsoe']+"}",""), leaf.text)
+
+
+                self.equipmentIDtoType[newKey] = equipmentType
+                newValue["associatedTerminals"] = []
+                self.equipment[equipmentType][newKey] = newValue
+
+    def runSSH(self):
+        for node in self.sshRoot:
+
+            #check equipment type
+            equipmentType = node.tag.replace("{"+ns['cim']+"}","")
+
+            #check ID of equipment
+            equipmentID = node.attrib.get(ns['rdf']+'about').replace("#", "")
+
+            if equipmentType in self.equipment.keys():
+                equipmentDict = self.equipment[equipmentType][equipmentID]
+                for leaf in node:
+                    #this is an rdf resource
+                    if leaf.text == None:
+                        attribute = leaf.tag.replace("{"+ns['cim']+"}","")
+                        value =leaf.attrib.get(ns['rdf']+'resource').replace("#", "")
+                        try:
+                            if equipmentDict[attribute] != value:
+                                oldValue = equipmentDict[attribute]
+                                equipmentDict[attribute] = value
+                                # print(attribute, "changed from", oldValue, "to", equipmentDict[attribute])
+                        except:
+                            equipmentDict[attribute] = value
+                            # print(attribute, "added:",  equipmentDict[attribute])
+                    #this is just normal text information
+                    else:
+                        attribute = leaf.tag.replace("{"+ns['cim']+"}","").replace("{"+ns['entsoe']+"}","")
+                        value = leaf.text
+                        try:
+                            if equipmentDict[attribute] != value:
+                                oldValue = equipmentDict[attribute]
+                                equipmentDict[attribute] = value
+                                # print(attribute, "changed from", oldValue, "to", equipmentDict[attribute])
+                        except:
+                            equipmentDict[attribute] = value
+                            # print(attribute, "added:",  equipmentDict[attribute])
+
+            else:
+                pass
+                # print("Not interested in", equipmentType)
+
+
+    def run(self):
+
+        self.runEQ()
+        self.associateTerminaltoEquipment()
+        self.runSSH()
+
+        return self.equipmentIDtoType, self.equipment
---
+++ PandaPowerManager.py
@@ -0,0 +1,223 @@
+import pandapower as pp
+import pandapower.plotting as plt
+
+class PandaPowerWriter:
+    def __init__(self, dictEquipmentIDtoType, dictEquipment) -> None:
+        self.equipmentIDtoType = dictEquipmentIDtoType
+        self.equipment = dictEquipment
+        self.network = None
+        self.busDict = {}
+
+    def initialiseNetwork(self):
+        """
+        Creates empty pandapower network
+        """
+        net = pp.create_empty_network() 
+        self.network = net
+        # Implement equipment in pandapower
+        self.initialiseBuses()
+        self.initialiseLines()
+        self.initialiseSwitches()
+        self.initialiseLoads()
+        self.initialiseTransformer()
+        self.initialiseStaticGen()
+        self.initialiseMachines()
+        self.initialiseShunt()
+        self.initialiseWard()
+    
+    def getVoltageLevel(self, nodeID):
+        """
+        Returns a float of the voltage level associated with a node
+        """
+        voltageLevelID = self.equipment["ConnectivityNode"][nodeID]['ConnectivityNode.ConnectivityNodeContainer']
+        return float(self.equipment["VoltageLevel"][voltageLevelID.replace("#", "")]['IdentifiedObject.name'])
+
+    def toHTML(self, htmlFileName = "network.html"):
+        """
+        Outputs a HTML file in the htmlOutput folder of the current network
+        """
+        plt.to_html(self.network, "htmlOutput/"+htmlFileName)
+        return None
+
+    def getAssociatedSubstation(self, nodeID) :
+        """ 
+        Returns a string with the Name of a Substation related to some equipment
+        """
+        voltageLevelID = self.equipment[self.equipmentIDtoType[nodeID]][nodeID]['ConnectivityNode.ConnectivityNodeContainer']
+        substationID = self.equipment[self.equipmentIDtoType[voltageLevelID]][voltageLevelID]['VoltageLevel.Substation']
+        substationName = self.equipment[self.equipmentIDtoType[substationID]][substationID]['IdentifiedObject.name']
+        return substationName
+    
+    def getAssociatedBus(self, terminalID):
+        nodeID = self.equipment["Terminal"][terminalID]['Terminal.ConnectivityNode']
+        return nodeID
+
+            
+    def getLineMaxCurrent(self, lineID):
+        answer = None
+        for key, value in self.equipment["CurrentLimit"].items():
+            currentLimit = value["CurrentLimit.value"]
+            OperationalLimitID = value["OperationalLimit.OperationalLimitSet"]
+            terimnalID = self.equipment[self.equipmentIDtoType[OperationalLimitID]][OperationalLimitID]["OperationalLimitSet.Terminal"]
+            checkAgaisnt = self.equipment[self.equipmentIDtoType[terimnalID]][terimnalID]["Terminal.ConductingEquipment"]
+            if checkAgaisnt == lineID:
+                answer = currentLimit
+            
+        return float(answer)/1000
+
+    def initialiseBuses(self, newBusID = None):
+        """ 
+        Creates the buses
+        Defaults newBusID as None, but in case you pass it a variable,
+        it creates a new bus because of T1 _BE_V2-2 and T1_NL_V2
+        """
+        if newBusID == None:
+            counter = 0
+            for terminal, terminalInfo in self.equipment["ConnectivityNode"].items():
+                
+                print(terminalInfo)
+                node = pp.create_bus(self.network, name='Node %s' % counter, vn_kv=self.getVoltageLevel(terminal), type='n',
+                            origin_id = terminal, zone = self.getAssociatedSubstation(terminal))
+                counter += 1
+                self.busDict[terminal] = node
+        else:
+            node = pp.create_bus(self.network, name='Node %s' % len(self.busDict), type='n', vn_kv='nan',
+                    origin_id = newBusID)  
+            self.busDict[newBusID] = node 
+
+    def initialiseLines(self):
+        """ 
+        Creates the lines
+        """
+
+        for lineID, lineInfo in self.equipment["ACLineSegment"].items():
+            terminalID0, terminalID1 = lineInfo['associatedTerminals']
+
+            #if there is a KeyError here, it means that the connectivity node does not exist yet. 
+            #we create a new connectivity node then
+            try:
+                fromBus = self.busDict[self.getAssociatedBus(terminalID0)]
+            except:
+                self.initialiseBuses(self.getAssociatedBus(terminalID0))
+                fromBus = self.busDict[self.getAssociatedBus(terminalID0)]
+            try:
+                toBus = self.busDict[self.getAssociatedBus(terminalID1)]
+            except:
+                self.initialiseBuses(self.getAssociatedBus(terminalID1))
+                toBus = self.busDict[self.getAssociatedBus(terminalID1)]
+
+
+            #create line
+            pp.create_line_from_parameters(self.network, fromBus, toBus, 
+                                           length_km = float(lineInfo['Conductor.length']), 
+                                           r_ohm_per_km = float(lineInfo['ACLineSegment.r'])/float(lineInfo['Conductor.length']), 
+                                           x_ohm_per_km = float(lineInfo['ACLineSegment.x'])/float(lineInfo['Conductor.length']), 
+                                           c_nf_per_km = 1e9*float(lineInfo['ACLineSegment.bch'])/float(lineInfo['Conductor.length']), 
+                                           name= lineInfo['IdentifiedObject.name'], 
+                                           index=None, type=None, geodata=None, 
+                                           in_service=True, 
+                                           df=1.0, parallel=1, 
+                                           max_i_ka = self.getLineMaxCurrent(lineID),
+                                           g_us_per_km=  1e6*float(lineInfo['ACLineSegment.gch'])/float(lineInfo['Conductor.length']), 
+                                           r0_ohm_per_km=float(lineInfo['ACLineSegment.r0'])/float(lineInfo['Conductor.length']), 
+                                           x0_ohm_per_km=float(lineInfo['ACLineSegment.x0'])/float(lineInfo['Conductor.length']), 
+                                           c0_nf_per_km= 1e9*float(lineInfo['ACLineSegment.b0ch'])/float(lineInfo['Conductor.length']), 
+                                           g0_us_per_km= 1e6*float(lineInfo['ACLineSegment.g0ch'])/float(lineInfo['Conductor.length']), 
+                                           origin_id = lineID)
+
+    def initialiseSwitches(self):
+        """ 
+        Creates the switches
+        """
+        for switchID, switchInfo in self.equipment["Breaker"].items():
+            terminalID0, terminalID1 = switchInfo['associatedTerminals']
+            fromBus = self.busDict[self.getAssociatedBus(terminalID0)]
+            toBus = self.busDict[self.getAssociatedBus(terminalID1)]
+            
+
+            #watch for the strange double negative here: normalOpen == true means that isClosed = False
+            isClosed = True
+            if switchInfo['Switch.normalOpen'] == 'true':
+                isClosed = False
+            pp.create_switch(self.network, fromBus, toBus, et="b", type="CB", closed=isClosed)
+
+
+    def initialiseTransformer(self):
+        for transformerID, transformerInfo in self.equipment["PowerTransformer"].items():
+            if len(transformerInfo['associatedTerminals']) == 2:
+                terminalID0, terminalID1 = transformerInfo['associatedTerminals']
+                hvBus = self.busDict[self.getAssociatedBus(terminalID0)]
+                lvBus = self.busDict[self.getAssociatedBus(terminalID1)]
+                pp.create_transformer(self.network, hvBus, lvBus, name=transformerInfo['IdentifiedObject.name'], 
+                std_type="25 MVA 110/20 kV", origin_id = transformerID)
+            
+            elif len(transformerInfo['associatedTerminals']) == 3:
+                terminalID0, terminalID1, terminalID2 = transformerInfo['associatedTerminals']
+                hvBus = self.busDict[self.getAssociatedBus(terminalID0)]
+                mvBus = self.busDict[self.getAssociatedBus(terminalID1)]
+                lvBus = self.busDict[self.getAssociatedBus(terminalID2)]
+                pp.create_transformer3w(self.network, hvBus, mvBus, lvBus, std_type = '63/25/38 MVA 110/20/10 kV',
+                 name=transformerInfo['IdentifiedObject.name'], origin_id = transformerID)
+
+
+    
+    def initialiseLoads(self):
+        for loadID, loadInfo in self.equipment["EnergyConsumer"].items():
+            terminalID = loadInfo['associatedTerminals'][0]
+            atBus = self.busDict[self.getAssociatedBus(terminalID)]
+            pp.create_load(self.network, atBus, p_mw= loadInfo['EnergyConsumer.p'], q_mvar=loadInfo['EnergyConsumer.q'], scaling=1.0, name= loadInfo['IdentifiedObject.name'], origin_id = loadID)
+
+
+    def initialiseStaticGen(self):
+        for generatorID, generatorInfo in self.equipment["EnergySource"].items():
+            terminalID = generatorInfo['associatedTerminals'][0]
+            atBus = self.busDict[self.getAssociatedBus(terminalID)]
+            pp.create_sgen(self.network , atBus, p_mw= -float(generatorInfo['EnergySource.activePower']), 
+                           q_mvar= -float(generatorInfo['EnergySource.reactivePower']), 
+                           name= generatorInfo['IdentifiedObject.name'],
+                           origin_id= generatorID, 
+                           rx = float(generatorInfo['EnergySource.r']))/float(generatorInfo['EnergySource.x'])
+            
+
+    def initialiseMachines(self):
+         for generatorID, generatorInfo in self.equipment["SynchronousMachine"].items():
+            terminalID = generatorInfo['associatedTerminals'][0]
+            atBus = self.busDict[self.getAssociatedBus(terminalID)]
+            pp.create_gen(self.network, atBus, p_mw= -float(generatorInfo['RotatingMachine.p']),
+                           max_q_mvar= float(generatorInfo['SynchronousMachine.maxQ']), 
+                           min_q_mvar= float(generatorInfo['SynchronousMachine.minQ']), 
+                           vm_pu= float(self.getVoltageLevel(self.getAssociatedBus(terminalID)))/float(generatorInfo['RotatingMachine.ratedU']), 
+                           sn_mva = float(generatorInfo['RotatingMachine.ratedS']),
+                           name= generatorInfo['IdentifiedObject.name'],
+                           origin_id = generatorID)      
+
+    def initialiseShunt(self):
+         for shuntID, shuntInfo in self.equipment["LinearShuntCompensator"].items():
+            terminalID = shuntInfo['associatedTerminals'][0]
+            atBus = self.busDict[self.getAssociatedBus(terminalID)]
+            q = 0
+            if float(shuntInfo['LinearShuntCompensator.bPerSection']) > 0:
+                q = float(shuntInfo['LinearShuntCompensator.bPerSection'])*float(shuntInfo['ShuntCompensator.normalSections'])
+                q = -float(shuntInfo['ShuntCompensator.nomU'])**2*q
+            
+            p = 0
+            if float(shuntInfo['LinearShuntCompensator.gPerSection']) > 0:
+                p = float(shuntInfo['LinearShuntCompensator.gPerSection'])*float(shuntInfo['ShuntCompensator.normalSections'])
+                p = -float(shuntInfo['ShuntCompensator.nomU'])**2*p
+            pp.create_shunt(self.network, atBus, 
+                            q_mvar= q, 
+                            p_mw= p, 
+                            name=shuntInfo['IdentifiedObject.name'], 
+                            origin_id= shuntID)       
+
+    def initialiseWard(self):
+         for wardID, wardInfo in self.equipment["EquivalentInjection"].items():
+            terminalID = wardInfo['associatedTerminals'][0]
+            atBus = self.busDict[self.getAssociatedBus(terminalID)]
+            pp.create_ward(self.network, atBus, 
+                           ps_mw = float(wardInfo['EquivalentInjection.p']), 
+                           qs_mvar = float(wardInfo['EquivalentInjection.q']), 
+                           pz_mw = float(wardInfo['EquivalentInjection.r']), 
+                           qz_mvar = float(wardInfo['EquivalentInjection.r']), 
+                           name= wardInfo['IdentifiedObject.name'],
+                           origin_id = wardID)        
